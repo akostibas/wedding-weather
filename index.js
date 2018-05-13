e0804064b9e96397e43a2fc2d3072a388178f3ce
Alexi Kostibas <alexi@kostibas.com>
2018-05-13 00:56:16 -0500

    changing target date

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const util = require('util');
 const screenshotDir = 'screenshots';
 const urlTemplate = 'https://www.accuweather.com/en/us/portola-ca/94134/daily-weather-forecast/2628246?day=%d#';
 
-const targetDate = new Date('June 16, 2018');
+const targetDate = new Date('June 17, 2018');
 const today = new Date(Date.now());
 const daysTillTarget = Math.ceil((targetDate - today) / 1000 / 60 / 60 / 24);
 
@@ -29,10 +29,10 @@ fs.stat(screenshotDir, (e, s) => {
 });
 
 (async () => {
-  console.log('taking screenshot of ', url);
+  //console.log('taking screenshot of ', url);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({width: 1000, height: 1500});
+  await page.setViewport({width: 800, height: 1500});
 
   await page.goto(url);
   await page.screenshot({path: filename});
